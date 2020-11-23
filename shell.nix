4bd5e3fe4a44cc3199f68c1a61caf9ed36b19e20
Matteo Joliveau <matteo.joliveau@mikamai.com>
2020-11-23 19:01:18 +0100

    Refactor bootstrapper
    Signed-off-by: Matteo Joliveau <matteo.joliveau@mikamai.com>

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
 let
   pkgs = import <nixpkgs> {};
+  openshift = import ./.nix/openshift.nix { pkgs = pkgs; };
+  pypkgs = pkgs.python38Packages;
 in
 pkgs.mkShell {
     buildInputs = with pkgs; [
       mkdocs
       ansible_2_10
+      openshift
+      pypkgs.kubernetes
     ];
 }
