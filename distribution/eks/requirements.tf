1ec2e93dc488f4f9f3838bce8d07b91a532c6385
Matteo Joliveau <matteo.joliveau@mikamai.com>
2020-12-02 17:55:13 +0100

    Add IAM role for external secrets to example
    Signed-off-by: Matteo Joliveau <matteo.joliveau@mikamai.com>

--- distribution/eks/requirements.tf
+++ distribution/eks/requirements.tf
@@ -4,5 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.14"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.13"
+    }
   }
 }
