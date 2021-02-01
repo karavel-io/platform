a383caa4e489834067a9a9bc6322796942a66a0f
Matteo Joliveau <matteojoliveau@gmail.com>
2021-02-01 10:30:51 +0100

    Begin exploding the upstream manifests
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   pkgs = import <nixpkgs> {};
-  openshift = import ./.nix/openshift.nix { pkgs = pkgs; };
+  openshift = pkgs.callPackage .nix/openshift.nix { inherit pkgs; };
+  linkerd = pkgs.callPackage .nix/linkerd.nix { inherit pkgs; };
   pypkgs = pkgs.python38Packages;
 in
 pkgs.mkShell {
@@ -9,5 +10,6 @@ pkgs.mkShell {
       ansible_2_10
       openshift
       pypkgs.kubernetes
+      linkerd
     ];
 }
