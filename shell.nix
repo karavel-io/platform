c1c0894763818bfbea30bf1c45bdf803f5a09da6
Matteo Joliveau <matteojoliveau@gmail.com>
2021-02-02 18:25:03 +0100

    Add loki and promtail dashboards and rules
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   pkgs = import <nixpkgs> {};
-  openshift = import ./.nix/openshift.nix { pkgs = pkgs; };
+  openshift = pkgs.callPackage ./.nix/openshift.nix { inherit pkgs; };
+  jb = pkgs.callPackage ./.nix/jb.nix { inherit pkgs; };
   pypkgs = pkgs.python38Packages;
 in
 pkgs.mkShell {
@@ -9,5 +10,7 @@ pkgs.mkShell {
       ansible_2_10
       openshift
       pypkgs.kubernetes
+      jsonnet
+      jb
     ];
 }
