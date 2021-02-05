4d64c3486635cec27d96162012982bf0a601085f
Matteo Joliveau <matteojoliveau@gmail.com>
2021-02-05 12:08:42 +0100

    Implement initializer with release download
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- cli/cmd/karavel/init.go
+++ cli/cmd/karavel/init.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/mikamai/karavel/cli/pkg/action"
+	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
 	"log"
 	"path/filepath"
@@ -11,24 +11,54 @@ import (
 
 func NewInitCommand(logger *log.Logger) *cli.Command {
 	var ver string
+	var filename string
+	var force bool
+	var cfgUrl string
+	var sumUrl string
 
 	return &cli.Command{
 		Name:      "init",
 		Usage:     "Initialize a new Karavel project",
 		ArgsUsage: "WORKDIR",
 		Flags: []cli.Flag{
-			&cli.PathFlag{
+			&cli.StringFlag{
 				Name:        "version",
 				Aliases:     []string{"v"},
 				Usage:       "Karavel Platform version to initialize",
 				Value:       "latest",
 				Destination: &ver,
 			},
+			&cli.PathFlag{
+				Name:        "config",
+				Aliases:     []string{"c"},
+				Usage:       "Karavel config file name to create",
+				Value:       DefaultFileName,
+				Destination: &filename,
+			},
+			&cli.BoolFlag{
+				Name:        "force",
+				Aliases:     []string{"f"},
+				Usage:       "Overwrite the config file even if it already exists",
+				Value:       false,
+				Destination: &force,
+			},
+			&cli.PathFlag{
+				Name:        "config-url",
+				Usage:       "URL pointing to the Karavel config file to download. Requires setting --checksum-url too",
+				DefaultText: "the official Karavel config file URL",
+				Destination: &cfgUrl,
+			},
+			&cli.PathFlag{
+				Name:        "checksum-url",
+				Usage:       "URL pointing to the Karavel config file checksum to download. Requires setting --config-url too",
+				DefaultText: "the official Karavel config file checksum URL",
+				Destination: &sumUrl,
+			},
 		},
 		Action: func(ctx *cli.Context) error {
 			cwd := ctx.Args().Get(0)
 			if cwd == "" {
-				return fmt.Errorf("argument 'workdir' must be provided")
+				return errors.New("argument 'workdir' must be provided")
 			}
 			cwd, err := filepath.Abs(cwd)
 			if err != nil {
@@ -37,9 +67,17 @@ func NewInitCommand(logger *log.Logger) *cli.Command {
 
 			ver = strings.TrimPrefix(ver, "v")
 
+			if (cfgUrl != "" && sumUrl == "") || (sumUrl != "" && cfgUrl == "") {
+				return errors.New("both --config-url and --checksum-url must be provided")
+			}
+
 			return action.Initialize(logger, action.InitParams{
-				Workdir:        cwd,
-				KaravelVersion: ver,
+				Workdir:         cwd,
+				Filename:        filename,
+				KaravelVersion:  ver,
+				Force:           force,
+				FileUrlOverride: cfgUrl,
+				SumUrlOverride:  sumUrl,
 			})
 		},
 	}
